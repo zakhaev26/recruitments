7be60b6f632974ef4dbaf496f212f613e0d7cf44
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-04-21 06:12:27 +0530

    dynamic porting

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/zakhaev26/recruitments/internal/database"
 	"github.com/zakhaev26/recruitments/internal/routes"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
 	}
 
-	PORT := os.Getenv("PORT")
 
 	database.InitAuth()
 	r := routes.Router()
-	http.ListenAndServe(PORT, r)
+	http.ListenAndServe(":"+port, r)
 }
